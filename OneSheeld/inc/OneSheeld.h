0d5c7755ff830904143177a35faa0e35b621eddc
arduinocodedog <arduinocodedog@gmail.com>
2016-01-27 09:03:25 -0600

    v1.8
    This update brings the pcDuino 1Sheeld Library in line with v1.8 of the
    Arduino Library including addition of the Vibration Shield and samples.

--- OneSheeld/inc/OneSheeld.h
+++ OneSheeld/inc/OneSheeld.h
@@ -42,7 +42,7 @@ typedef unsigned char byte;
 #define START_OF_FRAME  0xFF
 #define END_OF_FRAME 	0x00
 //Library Version
-#define LIBRARY_VERSION 9
+#define LIBRARY_VERSION 10
 //Time between sending Frames
 #define TIME_GAP		200UL
 
@@ -65,7 +65,7 @@ typedef unsigned char byte;
 
 
 //Numer of Shields
-#define SHIELDS_NO	41
+#define SHIELDS_NO	42
 
 //Class for Datalength and Data
 class FunctionArg
